dc9757955e8999764c63413ef29d58c09da7f141
Andrew McNamara <andrew.mcnamara@gmail.com>
2019-06-01 14:49:24 +1000

    Updates

---
+++ precache-manifest.150cbf25c560690160d624e32c4bd340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8601aa28f4d0d95db512498748bafc6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dddb5cffb6788db6e731",
+    "url": "/static/css/main.5bf7a745.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "dddb5cffb6788db6e731",
+    "url": "/static/js/main.28647029.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
